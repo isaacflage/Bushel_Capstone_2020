2ec775657bdf30c173b0cf66e942e16001695c00
isaacflage <35605929+isaacflage@users.noreply.github.com>
2020-02-12 13:52:08 -0600

    Changed updated_at to match created_at

--- DataGeneration.js
+++ DataGeneration.js
@@ -13,7 +13,7 @@ function generateSplit() {
         amount: (Math.floor((Math.random() * 100) + 1)), 
         amount_type: 'percentage',
         created_at: today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate() + ':' + today.getHours() + ':' + today.getMinutes(),
-        updated_at: null,
+        updated_at: today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate() + ':' + today.getHours() + ':' + today.getMinutes(),
         user_id: (Math.floor((Math.random() * 9999999) +1)),
         user_name: faker.name.findName()
     }
