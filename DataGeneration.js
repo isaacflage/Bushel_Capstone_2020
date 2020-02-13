fbb659dc1fce582eea39d61df43a4242e28fd3d8
Benjamin Albertson <benalbertson1@hotmail.com>
2020-02-12 19:37:38 -0600

    Completed ticket generator.

--- DataGeneration.js
+++ DataGeneration.js
@@ -1,73 +1,56 @@
 let faker = require('faker');
 
-function generateTicket(){
-    let ticket = {
-        id: 999552, 
-        version: "1.1.0", 
-        commodity_id: "sb",
-        elevator_id: "GRAN0001",
-        display_id: 999552,
-        identifier: 999552,
-        user_id: 9090551,
-        created_at: new Date(2019, 10, 28, 13, 10, 15),
-        updated_at: new Date(2019, 10, 28, 13, 10, 15),
-        deleted_at: null,
+function generateTicket(){   
+    remarkAmount = Math.floor(Math.random() * 10);
+    originalRemarks = [];
+    for (let i = 0; i < remarkAmount; i++) {
+        originalRemarks[i] = {
+            grade_factor: faker.hacker.noun(),
+            grade_factor_code: faker.hacker.abbreviation(),
+            grade_factor_value: 12.0 - ((Math.random() * 10) + 1).toFixed(2),
+            dock_value: 10.0 + ((Math.random() * 10) + 1).toFixed(2),
+            dock_unit: ((Math.random() * 10) + 1).toFixed(2)
+        } 
+    }
+    
+    let ticket = {      
+        id: Math.floor(Math.random() * 999999) + 1, 
+        version: Math.floor(Math.random() * 10) + "." + Math.floor(Math.random() * 10) + "." + Math.floor(Math.random() * 10), 
+        commodity_id: faker.address.stateAbbr(),
+        elevator_id: "GRAN000" + (Math.floor(Math.random() * 9) + 1),
+        display_id: Math.floor(Math.random() * 999999) + 1,
+        identifier: Math.floor(Math.random() * 999999) + 1,
+        user_id: Math.floor(Math.random() * 9999999) + 1,
+        created_at: faker.date.recent(),
+        updated_at: faker.date.recent(),
+        deleted_at: faker.date.recent(),
 
-        primary_dock_weight: 4.96,
-        primary_gross_weight: 495.67,
-        primary_net_weight: 423.24,
-        primary_shrink_weight: 67.47,
-        primary_tare_weight: null,
-        primary_weight_uom: "BU",
-        secondary_dock_weight: 1.0,
-        secondary_gross_weight: 42202.0,
-        secondary_net_weight: 24199.0,
-        secondary_shrink_weight: 12.75,
-        secondary_tare_weight: 14711.0,
-        secondary_weight_uom: "lbs",
+        primary_dock_weight: ((Math.random() * 10) + 1).toFixed(2),
+        primary_gross_weight: (495.67 - (Math.random() * 100) + 1).toFixed(2),
+        primary_net_weight: (423.24 - (Math.random() * 100) + 1).toFixed(2),
+        primary_shrink_weight: (67.47 - (Math.random() * 10) + 1).toFixed(2),
+        primary_tare_weight: ((Math.random() * 100) + 1).toFixed(2),
+        primary_weight_uom: faker.address.stateAbbr(),
+        secondary_dock_weight: ((Math.random() * 10) + 1).toFixed(2),
+        secondary_gross_weight: (42202.0 - (Math.random() * 100) + 1).toFixed(2),
+        secondary_net_weight: (24199.0 - (Math.random() * 100) + 1).toFixed(2),
+        secondary_shrink_weight: (12.75 - (Math.random() * 10) + 1).toFixed(2),
+        secondary_tare_weight: (14711.0 - (Math.random() * 100) + 1).toFixed(2),
+        secondary_weight_uom: (Math.random() < 0.5) ? "lbs":"kgs",
 
-        remarks: [
-            {
-                grade_factor: "Test Weight",
-                grade_factor_code: "TW",
-                grade_factor_value: 12.0,
-                dock_value: null,
-                dock_unit: null
-            },
-            {
-                grade_factor: "Moisture",
-                grade_factor_code: "Moisture",
-                grade_factor_value: 10.6,
-                dock_value: null,
-                dock_unit: null
-            },
-            {
-                grade_factor: "damage",
-                grade_factor_code: "DAMAGE",
-                grade_factor_value: 3.2,
-                dock_value: null,
-                dock_unit: null
-            },
-            {
-                grade_factor: "Monetary moisture discount",
-                grade_factor_code: "Mon MS",
-                grade_factor_value: 11.6,
-                dock_value: null,
-                dock_unit: null
-            }
-        ],
+        remarks: originalRemarks,
 
-        sample_id: null,
-        truck_driver: null,
-        truck_license: null,
-        truck_name: null,
-        weigh_in_at: null,
-        weigh_master: null,
-        weigh_out_at: null,
-        field_name: null,
-        field_id: null,
-        grader: null,
-        lot_id: null
+        sample_id: faker.lorem.word(),
+        truck_driver: faker.name.firstName() + " " + faker.name.lastName(),
+        truck_license: faker.random.uuid(),
+        truck_name: "Semi " + Math.floor((Math.random() * 10) + 1),
+        weigh_in_at: faker.date.recent(),
+        weigh_master: faker.name.firstName() + " " + faker.name.lastName(),
+        weigh_out_at: faker.date.recent(),
+        field_name: faker.random.word(),
+        field_id: faker.company.companySuffix(),
+        grader: faker.name.firstName() + " " + faker.name.lastName(),
+        lot_id: "L" + Math.floor((Math.random() * 10) + 1)
     };
 
     console.log(JSON.stringify(ticket));
