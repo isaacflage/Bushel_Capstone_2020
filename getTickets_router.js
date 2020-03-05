59025efb477f1db4f158f0d6d0db51d5d5a691cb
Benjamin Albertson <benalbertson1@hotmail.com>
2020-03-05 00:29:35 -0600

    made basic status endpoint that shows total amount of tickets

--- getTickets_router.js
+++ getTickets_router.js
@@ -3,12 +3,20 @@ var router = express.Router();
 const dataGen = require('./DataGeneration');
 const restApi = require('./RestAPI');
 
+//tracks total amount of tickets
+let ticketCount = 0;
 
 //query router
 router.get('/getTickets', (req, res) => {
   tickets = dataGen.getTickets(req.query.count);
   res.json(tickets);
   restApi.sendTickets(tickets);
+  
+  ticketCount += req.query.count;
+});
+
+router.get('/status', (req, res) => {
+  res.send('Number of tickets: ' + ticketCount)
 });
 
 // define the home page route
