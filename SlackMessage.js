2910c9c91bb2898dcbec90000cace7e2f1e8a852
smsilverberg <49730421+smsilverberg@users.noreply.github.com>
2020-04-10 00:11:20 -0500

    message update
    changed message from console input to argumment in slack.send() method

--- SlackMessage.js
+++ SlackMessage.js
@@ -8,18 +8,25 @@ var slack = new Slack(webHook);
 var channel = process.argv[2];
 var message = process.argv[3];
 
-console.log('Sending message', message, 'to channel', channel);
+console.log('Sending message to channel', channel);
 
 slack.send({
-    text: message,
+    //message will be updated to send 
+    text: 'it works!! (this is where error messages will go)',
     channel: '#' + channel
     //username: username
 });
-/**
+/**add to readme, fool!
+ * 
  * TO RUN:
- * 
+ * npm install node-slack
  * node SlackMessage <channel> <message>
- * 
  * where channel is desired channel. message must be one word
  * example: node SlackMessage general helloWorld!
+ * should work with any existing channel
+ * https://ajlopez.wordpress.com/2015/02/06/enviando-mensaje-a-slack-con-javascriptnode-js/
+ * 
+ * TODO/notes to self: figure out how i was able to send messages str8 from the 'test' slack app i created earlier
+ * i swear i didnt change it and it just stopped working (something to do with token instead of webhook probably)
+ * also figure out how to send a concatanated string instead of just one word (json formatting)
  */
